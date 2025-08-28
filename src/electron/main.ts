ac4ec78f73f0e7b26420c421e59278fe992c915a
Kevin Menard <kevin@nirvdrum.com>
2025-08-28 15:00:52 -0400

    Only open the web dev tools if the `SEAFOAM_DEVTOOLS` env var is set.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -57,8 +57,10 @@ const createWindow = (): void => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // Open the DevTools if SEAFOAM_DEVTOOLS is set.
+  if (process.env.SEAFOAM_DEVTOOLS) {
+    mainWindow.webContents.openDevTools();
+  }
 };
 
 // This method will be called when Electron has finished
